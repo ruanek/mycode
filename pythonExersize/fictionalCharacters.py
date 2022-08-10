5ccbf8f62c1d3c8398016d3902bf73b05179f9d0
Kyle Ruane <ruanek@users.noreply.github.com>
2022-08-10 19:56:42 +0000

    List of Lists and Dictionary class exersize

---
+++ pythonExersize/fictionalCharacters.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+hero_dict = {
+"paul" : {
+    "Full Name": "Paul Atreides", 
+    "House Affiliation": "Atreides", 
+    "Aliases": True},
+
+"jessica" : {
+    "Full Name": "Jessica NoLastName", 
+    "House Affiliation": "Atreides", 
+    "Aliases": False},
+
+"leto" : {
+    "Full Name": "Leto Atreides", 
+    "House Affiliation": "Atreides", 
+    "Aliases": True},
+
+"vladimir" : {
+    "Full Name": "Vladimir Harkonnen", 
+    "House Affiliation": "Harkonnen", 
+    "Aliases": True}
+}
+
+
+print(hero_dict.keys())
+print(hero_dict.values())
---
+++ pythonExersize/foods.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+typesList = ["Italian", "Thai", "Indian", "Mediterranean"]
+entreeList = ["Carbonara", "Pad Thai", "Lamb Vindaloo", "Gyro"]
+dessertList = ["Cannoli", "Mango Sticky Rice", "Kheer", "Baklava"]
+
+combinedList = [typesList, entreeList, dessertList]
+print(combinedList)
+
+print(combinedList[1] [2])
