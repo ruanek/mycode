12d66f6570a0a12ca243ab514fc0e5f19a34d686
Kyle Ruane <ruanek@users.noreply.github.com>
2022-08-11 17:09:29 +0000

    Updated logic in custom if

--- cust_if_script.py
+++ cust_if_script.py
@@ -6,7 +6,7 @@ connection = int(input("What is your score from  0 - 100 (enter whole numbers on
 
 if connection > 100:
     message = 'You are an overachiever, or you cannot read directions...'
-elif connection >= 90:
+elif connection >= 90 and connection <= 100:
     message = message + 'A, Great job keep up the great work.'
 elif connection >= 80:
     message = message + 'B, Good job keep working hard for that A.'
@@ -14,7 +14,7 @@ elif connection >= 70:
     message = message + 'C, OK you might need to do some more studying.'
 elif connection >= 60:
     message = message + 'D, Ouch you failed and should spend some more time studying.'
-elif connection <= 59:
+elif connection <= 59 and connection >= 0:
     message = message + 'F, This was not good please try harder.'
 else:
     message = 'Please read the directions again and enter a valid score.'
