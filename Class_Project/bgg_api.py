3dbfc0402fbedc2c109236080b3fc1b6e9d56cbc
Kyle Ruane <ruanek@users.noreply.github.com>
2022-08-18 04:48:37 +0000

    added error handling and exit functionality to search_specific_game()

--- Class_Project/bgg_api.py
+++ Class_Project/bgg_api.py
@@ -3,17 +3,18 @@
 
 import requests
 import xmltodict
+from pprint import pprint
 
 game_list = {}
 bgg_url =  "https://boardgamegeek.com/xmlapi"
 
-# function to call API with string seach
+# function to call API with string search
 def searchgame():
     """ enter something here """
 
     usrinput = input("What game are you looking for?: ")
     response = requests.get(f"{bgg_url}/search?search={usrinput}")
-    print(response.status_code)
+    
 
     dict_data = xmltodict.parse(response.content)["boardgames"]["boardgame"]
     counter = 0
@@ -26,39 +27,59 @@ def searchgame():
         game_list[game_id] = name
     for game in game_list:
         counter += 1
-        print(f"{counter} - Game: {game_list[game]}")
+        pprint(f"{counter} - Game: {game_list[game]}")
 
 # function to search specific game details using id's
-def specific_game():
+def search_specific_game():
     """ enter something here """
-
-    usrinput2 = input("Would you like to know more about any?: ")
-    input_list = usrinput2.split(",")
-
+    
+    usrinput = input("Looking for more info? Enter the # of the game separated by a comma (i.e 1 or 1,2,3), or type 'exit' to quit: ")
     keylist = list(game_list.keys())
-    for i, item in enumerate(input_list):
-        input_list[i] = keylist[(int(input_list[i]) - 1)]
-    print(input_list)
+    searchlist = None
 
-    searchlist = ",".join(input_list)
+    if usrinput == "exit":
+        print("Exiting...")
+    else:
+        if usrinput.__contains__(","):
+            input_list = usrinput.split(",")
+            for i, item in enumerate(input_list):
+                input_list[i] = keylist[(int(input_list[i]) - 1)]
+            searchlist = ",".join(input_list)
+        else:
+            input_list = usrinput
+            searchlist = str(keylist[(int(input_list) - 1)])
+            
+        response = requests.get(f"{bgg_url}/boardgame/{searchlist}")
+        
+        boardgame = xmltodict.parse(response.content)["boardgames"]["boardgame"]
+        
+        if isinstance(boardgame, dict):
+            boardgame = [boardgame]
 
-    response = requests.get(f"{bgg_url}/boardgame/{searchlist}")
-    print(response.status_code)
-
-    dict_data2 = xmltodict.parse(response.content)["boardgames"]["boardgame"]
-
-    for game in dict_data2:
-        game_id2 = (game.get("@objectid"))
-        yr_pub = (game.get("yearpublished"))
-        description = (game.get("description")).replace("<br/>", "")
-        print(f"Name: {game_list[game_id2]}({yr_pub}):\nDescription: {description}\n\n")
+        for game in boardgame:
+            game_id2 = (game.get("@objectid"))
+            yr_pub = (game.get("yearpublished"))
+            description = (game.get("description")).replace("<br/>", "").replace("&mdash", "")
+            print(f"\nName: {game_list[game_id2]}({yr_pub}):\nDescription:\n{description}\n\n")
 
 def main():
     """called at runtime"""
 
     print("Welcome to the gamefinder")
 
-    searchgame()
-    specific_game()
+    
+    while True:
+        try:
+            searchgame()
+            break
+        except:
+            print("Your Search did not return a response, Please try again")
+
+    while True:
+        try:
+            search_specific_game()
+            break
+        except:
+            print("Please select values within the list")
 
 main()
